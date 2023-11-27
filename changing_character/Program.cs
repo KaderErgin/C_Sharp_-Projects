e293a402520943a18ea25f28f8379c44b1bb343c
Kader Ergin <kader.ergin.37@gmail.com>
2023-11-27 03:33:53 +0300

    Create Program.cs

---
+++ changing_character/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Lüften bir metin giriniz: ");
+string input = Console.ReadLine();
+string output = "";
+string[] strings = input.Split(" ");
+
+foreach (var word in strings)
+{
+    char[] chars = word.ToCharArray();
+    char first = chars[0];
+    char last = chars[chars.Length - 1];
+    chars[0] = last;
+    chars[chars.Length - 1]=first;
+
+    output += new string(chars) + " ";
+
+
+
+}
+Console.Write("Output: " + output);
